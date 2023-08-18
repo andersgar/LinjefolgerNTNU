e2af11902a3521de0fff7e9bfdd91c294de90426
Anders Garberg <56233864+andersgar@users.noreply.github.com>
2023-08-18 10:11:56 +0200

    Cleanup

--- Linjefolger/Linjefolger.ino
+++ Linjefolger/Linjefolger.ino
@@ -7,9 +7,9 @@ bool turning = 0;
 
 int maxSpeed = 90;
 bool lineColor = BLACK;
-float turnConf = 0.75;
-float turnConf2 = 1.8;
-int proxConf = 10;
+float turnConf = 0.75; //svingekonstant 1
+float turnConf2 = 1.8; //svingegonstant 2
+int proxConf = 10; //prox-baseline
 
 void setup() {
   car.initCar();
@@ -18,31 +18,28 @@ void setup() {
 
 void loop() {
   int lineValue = car.data[LINE].value;
-  //car.sendData(1, lineValue);
-  //car.sendData(2, car.data[GYRO].value);
-  
+
   if(lineMode && !turning){
     float speedDifference = (lineValue*turnConf)+ turnConf2 * (lineValue - prevError);
     car.drive(maxSpeed + speedDifference, maxSpeed - speedDifference);
     prevError = lineValue;
   }
 
-  if (car.data[PROXIMITY].flag) {
+  if (car.data[PROXIMITY].flag) {//Proxplotter
     int proDATA = car.data[PROXIMITY].value;
     car.data[PROXIMITY].flag = false;
     car.sendData(2, proDATA); //sender proximity data til grafen 2 kontinuerlig ved void loop()
     if(proDATA >= proxConf){
-      triangle(DOWN);
+     // triangle(DOWN);  for å automatisk snu ved prox
     }
   }
  
-  if (car.data[ENCODERS].flag) {
+  if (car.data[ENCODERS].flag) {//Speedplotter
     float encDATA = car.data[ENCODERS].value;
     float tidmeasure = car.data[READ_TIME].value;
     //Hjulenes omkrets er 2*3.14*(19.5 mm) = 122.46 mm eller 0.12246 m, hvis man deler 75*12 (gear ratio and frequency of magnet rotation) på encDATA får man hvor mange omkretsen/hjulene gikk rundt hver 70 ms eller car.data[READ_TIME].value, får man fartsmålingen
     float gangerOmkrets = encDATA / (75 * 12);
     float fartDATA = (100 * 122.246 * gangerOmkrets) / tidmeasure; // i mm per sekund, men grafer m/s, så vi må gange med 100 for cm / s
-    //Serial.println(fartDATA);
     car.data[ENCODERS].flag = false; // Merk at enkoderene teller 12 ganger per motorrotasjon. Hjulene roterer  ́en gang per 75te motorrotasjon. Hjulenes diamater, inkludert beltene, er 39mm. 
     car.sendData(1, fartDATA);
   }
